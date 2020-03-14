2e343dff0a476bb3ff9f78a8be185c845ab64cce
Rahma bn <rahma.benafia123@gmail.com>
2020-03-14 17:16:08 +0100

    commit

--- assets/js/Components/Navbar.jsx
+++ assets/js/Components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = ({ history }) => {
           </li>
           <li className="nav-item">
             <NavLink className="nav-link" to="/invoices">
-              Factures
+             About
             </NavLink>
           </li>
                     <li className="nav-item">
